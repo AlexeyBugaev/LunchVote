86c8d78848533d59168bf0f405fbeac610ba84a4
AlexeyBugaev <alexeybugaevjava@gmail.com>
2019-02-24 11:50:17 +0700

    24/02/19: - getVoteHistory and clearVoteHistory were added

--- src/main/java/vote/web/controller/UserController.java
+++ src/main/java/vote/web/controller/UserController.java
@@ -8,11 +8,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import vote.model.User;
+import vote.model.VoteHistory;
+import vote.repository.CrudVoteHistoryRepository;
 import vote.service.UserService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
+import static vote.Utils.RestaurantUtil.setServletResponseSuccessMessage;
 import static vote.Utils.ValidationUtil.assureIdConsistent;
 
 @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -23,6 +28,9 @@ public class UserController {
 
     @Autowired
     protected UserService userService;
+
+    @Autowired
+    private CrudVoteHistoryRepository voteHistoryRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAll() {
@@ -56,6 +64,18 @@ public class UserController {
     public void update(@RequestBody User user, @PathVariable("id") int id) {
         assureIdConsistent(user, id);
         userService.update(user);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping(value = "/{id}/voteHistory", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<VoteHistory> getVoteHistory(@PathVariable int id) {
+        return voteHistoryRepository.findAll(id);
+    }
+
+    @GetMapping(value = "/clearVoteHistory", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void clearVoteHistory(HttpServletResponse httpServletResponse) throws IOException {
+        voteHistoryRepository.findAll().forEach(voteHistory -> voteHistoryRepository.delete(voteHistory));
+        setServletResponseSuccessMessage(httpServletResponse, "VoteHistoryClear");
     }
 
     @GetMapping(value = "/by", produces = MediaType.APPLICATION_JSON_VALUE)
